55423335291a3e3620c6a5f5b3e00e6aa48eac31
utama145 <utamanathan407@gmail.com>
2024-03-17 13:23:06 +0700

    first commit

---
+++ yy.js
@@ -0,0 +1,18 @@
+function scoreChecker(score) {
+  let result = 80;
+
+  // TODO
+  if (result >= 90) {
+    console.log('Selamat! Anda mendapatkan nilai A');
+  } else {
+    console.log('Maaf, Anda belum mendapatkan nilai A');
+  }
+
+  // Jangan hapus kode ini
+  return result;
+}
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+module.exports = scoreChecker;
